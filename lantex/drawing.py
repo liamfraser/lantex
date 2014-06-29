1bc511474e42f16df09f190fa30337a3ecb1f834
Liam Fraser <liam@liamfraser.co.uk>
2014-06-29 11:07:41 +0100

    Can load a color scheme

--- lantex/drawing.py
+++ lantex/drawing.py
@@ -1,6 +1,12 @@
 import svgwrite
+import json
 
 class Drawing():
+    def __init__(self):
+        with open('lantex/colors.json', 'r') as fh:
+            decoder = json.JSONDecoder()
+            self.colors = decoder.decode(fh.read())
+
     def render(self, output):
         dwg = svgwrite.Drawing(output, profile='tiny')
         # Drawing bits go here
